7588718697c154130f470a19b7c74f168329f599
Elena <lenalosyakova@gmail.com>
2022-10-08 18:59:21 +0300

    Cubes

---
+++ work1/Program.cs
@@ -0,0 +1,10 @@
+﻿//найти кубы чисел от 1 до N
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int M = 1;
+
+while (M <= N)
+{
+   Console.WriteLine("Кубы чисел: " + M * M * M);
+   M++;
+};
