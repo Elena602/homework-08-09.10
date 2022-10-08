a1c681fee5ff4b2700ae8f55e1c9a5adf7c80823
Elena <lenalosyakova@gmail.com>
2022-10-08 19:41:27 +0300

    multiplication

---
+++ work3/Program.cs
@@ -0,0 +1,11 @@
+﻿// написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int M = 1;  
+int multiplication = 1;
+while (M <= N)
+{
+    multiplication = multiplication * M;
+    M++;
+};
+Console.WriteLine("Произведение чисел: " + multiplication);
