451b50ba9a08ad3f2c239ad8630ce5588adc747e
Elena <lenalosyakova@gmail.com>
2022-10-08 19:12:26 +0300

    Cubes from even numbers

---
+++ work4/Program.cs
@@ -0,0 +1,13 @@
+﻿// показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int M = 1;
+
+while (M <= N)
+{
+    if (M % 2 == 0)
+    {
+    Console.WriteLine("Кубы чисел, заканчивающихся на четную цифру: " + M * M * M);
+    }
+    M++;
+};
