dae3a48b7df8058b4699af66cd0a5ae0d20a5de4
Elena <lenalosyakova@gmail.com>
2022-10-09 08:33:24 +0300

    sum of numbers

---
+++ work2/Program.cs
@@ -0,0 +1,11 @@
+﻿//посчитать сумму цифр в числе
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int sum = 0;
+N = Math.Abs(N);
+while (N > 0)
+{
+   sum = sum + N % 10;
+   N = N / 10;
+};
+Console.WriteLine("Сумма чисел: " + sum);
